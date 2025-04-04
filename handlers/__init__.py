7822f50ea3097fc6c7100ca0af8ddc245dc3188c
Milaigmn <millaigumnova@gmail.com>
2025-04-04 19:36:59 +0300

    Добавила меню команд с командами start status help

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,3 @@
-# Файл __init__.py.py позволяет обращаться к папке как к модулю
-# и импортировать из него содержимое
-
-from.handlers import router
-from .bot_commands import set_my_commands
-
+from .bot_commands import set_commands
+from .handlers import router
 
