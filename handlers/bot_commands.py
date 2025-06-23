ac763f66af968bb798ae32a42f726d4362b7048a
Mila18-star <melaniaivankova@yandex.ru>
2025-06-24 01:06:12 +0300

    Добавила команды vmpath, check

--- handlers/bot_commands.py
+++ handlers/bot_commands.py
@@ -2,8 +2,10 @@ from aiogram.types import BotCommand, BotCommandScopeDefault
 
 async def set_commands(bot):
     commands = [
-        BotCommand(command='start', description='Старт'),
-        BotCommand(command='status', description='Информация о пользователе'),
-        BotCommand(command='help', description='Справка'),
+        BotCommand(command='start', description='Start'),
+        BotCommand(command='status', description='User Information'),
+        BotCommand(command='help', description='Help'),
+        BotCommand(command='vmpath', description='Saving a VM'),
+        BotCommand(command='check', description='Checking the connection'),
     ]
-    await bot.set_my_commands(commands, BotCommandScopeDefault())
+    await bot.set_my_commands(commands, scope=BotCommandScopeDefault())
