a450cbc40d1438af3eccdfe6ed991513856d366c
Mila18-star <melaniaivankova@yandex.ru>
2025-06-26 17:16:42 +0300

    Импортировала base

--- db/__init__.py
+++ db/__init__.py
@@ -1,2 +1,5 @@
 from .models import User
-from .engine import async_session, async_create_table
+from .engine import async_session, async_create_table, engine
+from .base import Base
+
+__all__ = ["User", "Base", "async_session", "async_create_table", "engine"]
