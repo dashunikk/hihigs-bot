dc4003b1e55df0300eb4cd08814ae8c7e8adf9f1
dashunikk <dariashhh5@gmail.com>
2025-06-26 10:52:39 +0300

    добавила в боткоммандс лс

--- handlers/bot_commands.py
+++ handlers/bot_commands.py
@@ -7,5 +7,6 @@ async def set_commands(bot):
         BotCommand(command='help', description='Help'),
         BotCommand(command='vmpath', description='Saving a VM'),
         BotCommand(command='check', description='Checking the connection'),
+        BotCommand(command='ls', description='List directory contents'),
     ]
     await bot.set_my_commands(commands, scope=BotCommandScopeDefault())
